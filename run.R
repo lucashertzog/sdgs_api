29ea4dc547bbf0ccbcd528956b28361afd886f04
Lucas Hertzog <291828H@curtin.edu.au>
2024-05-03 13:45:40 +0800

    sdfinfo

--- run.R
+++ run.R
@@ -8,3 +8,8 @@ do_clean()
 
 # 3 Generate and interactive plot with the data cleaned
 do_plot()
+
+
+### Resources ####
+# https://unstats.un.org/sdgapi/swagger/
+# https://unstats.un.org/sdgs/indicators/SDG_Updateinfo.xlsx
