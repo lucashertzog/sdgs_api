3f45c4adb5090646e6cb831bc7a02177ba99e7cd
Lucas Hertzog <291828H@curtin.edu.au>
2024-05-03 12:16:28 +0800

    ..

--- config.R
+++ config.R
@@ -1,3 +1,4 @@
+# packages
 
 if (!require(plotly)) {
   install.packages("plotly")
@@ -9,6 +10,7 @@ if (!require(data.table)) {
   library(data.table)
 }
 
+## set folder names
 folder_names <- c("data_derived", "data_provided")
 
 for (folder_name in folder_names) {
@@ -19,3 +21,12 @@ for (folder_name in folder_names) {
     cat("Folder", folder_name, "already exists.\n")
   }
 }
+
+## source functions
+file_list <- list.files(path = "R", pattern = "\\.R$", full.names = TRUE)
+
+# Source each .R file
+for (file in file_list) {
+  source(file)
+}
+
